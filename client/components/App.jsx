7e5c59567cfd28733dd3152d14c69fd4c30962ef
Thomas Rose <thomascrose90@gmail.com>
2021-12-03 15:31:10 -0500

    Remove index.html from gitignore. Update App component to make axios call for all holidays on mounting.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      holidays: []
     }
+    this.getHolidays = this.getHolidays.bind(this);
+  }
+
+  componentDidMount() {
+    this.getHolidays();
+  }
+
+  getHolidays() {
+    axios.get('/')
+      .then(results => {
+        console.log('GET RESULTS: ', results);
+      })
+      .catch(err => {
+        console.log('GET Holidays error: ', err);
+      })
   }
 
   render() {
